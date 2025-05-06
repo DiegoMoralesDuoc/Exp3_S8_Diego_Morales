31bd7012cec22ba229e6cdf886d2dacd4b94c456
DiegoMoralesDuoc <dieg.moralesa@duocuc.cl>
2025-05-05 22:45:59 -0400

    class ViajesMascotaApplication added

---
+++ src/main/java/com/fullstack/clases/ViajesMascotaApplication.java
@@ -0,0 +1,12 @@
+package com.fullstack.clases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesMascotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViajesMascotaApplication .class, args);
+    }
+}
